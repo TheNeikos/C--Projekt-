fc67cfd37fb2a6df15762b9c316555d8dd5c6566
Marcel Müller <neikos@neikos.email>
2015-04-14 17:22:00 +0200

    Added initial Layout

--- CSharpProjekt/Form1.cs
+++ CSharpProjekt/Form1.cs
@@ -16,5 +16,10 @@ namespace CSharpProjekt
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
