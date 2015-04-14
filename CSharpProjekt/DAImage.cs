85c9f055554e57b4c5933c556b5f032e08cc44be
Mario5h4h4 <mario-krehl@gmx.de>
2015-04-14 17:15:15 +0200

    added daimage class
    skeleton and some simple variables that will most likely be needed

---
+++ CSharpProjekt/DAImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpProjekt
+{
+    class DAImage
+    {
+        public string d_ID;
+        public string title;
+        public string category;
+        public string author;
+        string img_url;
+        public int height;
+        public int width;
+        string thumbnail_url;
+    }
+}
